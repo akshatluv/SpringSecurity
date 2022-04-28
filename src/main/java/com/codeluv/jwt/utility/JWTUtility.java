2f973ff2b07cda6fc7b8dd44a0591935478376cc
akshatluv <akshat248raj@gmail.com>
2022-04-28 22:47:31 +0530

    initial phase

---
+++ src/main/java/com/codeluv/jwt/utility/JWTUtility.java
@@ -0,0 +1,9 @@
+package com.codeluv.jwt.utility;
+
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class JWTUtility implements Serializable {
+}
